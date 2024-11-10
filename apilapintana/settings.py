4a605032efb28b638490a6f2a1ce0e2968eca3b8
Nestor Carvacho <98226435+NestorCarvacho@users.noreply.github.com>
2024-11-10 17:40:12 -0300

    Update settings.py
    se quita api key

--- apilapintana/settings.py
+++ apilapintana/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3$&)7#60b%cej$6pdr_oia2oh6lj16_tv)a#ogfn)uvh)!n+#9'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
